e28a2020249be2416cbb554a4b8a1133ff7a3f0a
Connor Aird <c.aird@ucl.ac.uk>
2025-06-09 11:53:06 +0100

    Cleanup main src and rename file

--- episodes/2-intro-to-fortran-unit-tests/challenge-1/src/game_of_life.f90
+++ episodes/2-intro-to-fortran-unit-tests/challenge-1/src/game_of_life.f90
@@ -11,24 +11,24 @@ program game_of_life
     !! Board args
     integer, parameter :: max_nrow = 100, max_ncol = 100, max_generations = 100
     integer :: nrow, ncol
-    integer :: i, generation_number, sum, steady_state_generation
-    integer, dimension(:,:), allocatable :: board, temp_board
+    integer :: row, generation_number
+    integer, dimension(:,:), allocatable :: current_board, new_board
 
     !! Animation args
     integer, dimension(8) :: date_time_values
-    integer :: mod_ms_step, ms_per_step = 250, steady_state_counter = 0
+    integer :: mod_ms_step, ms_per_step = 250
     logical :: steady_state = .false.
 
     !! CLI args
     integer                       :: argl
-    character(len=:), allocatable :: a, input_fname
+    character(len=:), allocatable :: cli_arg_temp_store, input_fname
 
     !! File IO args
-    character(len=80) :: text
+    character(len=80) :: text_to_discard
     integer :: input_file_io
     integer :: iostat
 
-    ! Get board file path from command line
+    ! Get current_board file path from command line
     if (command_argument_count() == 1) then
         call get_command_argument(1, length=argl)
         allocate(character(argl) :: input_fname)
@@ -36,10 +36,10 @@ program game_of_life
     else
         write(*,'(A)') "Error: Invalid input"
         call get_command_argument(0, length=argl)
-        allocate(character(argl) :: a)
-        call get_command_argument(0, a)
-        write(*,'(A,A,A)') "Usage: ", a, " <input_file_name>"
-        deallocate(a)
+        allocate(character(argl) :: cli_arg_temp_store)
+        call get_command_argument(0, cli_arg_temp_store)
+        write(*,'(A,A,A)') "Usage: ", cli_arg_temp_store, " <input_file_name>"
+        deallocate(cli_arg_temp_store)
         stop
     end if
 
@@ -54,45 +54,49 @@ program game_of_life
         stop 1
     end if
 
-    ! Read in board from file
-    read(input_file_io,'(a)') text ! Skip first line
+    ! Read in current_board from file
+    read(input_file_io,'(a)') text_to_discard ! Skip first line
     read(input_file_io,*) nrow, ncol
 
     ! Verify the date_time_values read from the file
     if (nrow < 1 .or. nrow > max_nrow) then
-        write (*,'(a,i6,a,i6)') "nrow must be a positive integer less than ", max_nrow, " found ", nrow
+        write (*,'(a,i6,a,i6)') &
+            "nrow must be cli_arg_temp_store positive integer less than ", max_nrow, " found ", nrow
         stop 1
     end if
 
     if (ncol < 1 .or. ncol > max_ncol) then
-        write (*,'(a,i6,a,i6)') "ncol must be a positive integer less than ", max_ncol, " found ", ncol
+        write (*,'(a,i6,a,i6)') &
+            "ncol must be cli_arg_temp_store positive integer less than ", max_ncol, " found ", ncol
         stop 1
     end if
 
-    allocate(board(nrow, ncol))
-    allocate(temp_board(nrow, ncol))
+    allocate(current_board(nrow, ncol))
+    allocate(new_board(nrow, ncol))
 
-    read(input_file_io,'(a)') text ! Skip next line
+    read(input_file_io,'(a)') text_to_discard ! Skip next line
     ! Populate the boards starting state
-    do i = 1, nrow
-        read(input_file_io,*) board(i, :)
+    do row = 1, nrow
+        read(input_file_io,*) current_board(row, :)
     end do
 
-    temp_board = 0
+    close(input_file_io)
+
+    new_board = 0
 
     ! Clear the terminal screen
     call system ("clear")
 
-    ! Iterate until we reach a steady state
+    ! Iterate until we reach cli_arg_temp_store steady state
     do while(.not. steady_state .and. generation_number < max_generations)
         ! Advance the simulation in the steps of the requested number of milliosecons
         call date_and_time(VALUES=date_time_values)
         mod_ms_step = mod(date_time_values(8), ms_per_step)
 
         if (mod_ms_step == 0) then
-            call evolve_board()
             call check_for_steady_state()
-            board = temp_board
+            call evolve_board()
+            current_board = new_board
             call draw_board()
 
             generation_number = generation_number + 1
@@ -106,56 +110,28 @@ program game_of_life
         write(*,'(a,i6,a)') "Did NOT Reach steady after ", generation_number, " generations"
     end if
 
-    deallocate(board)
-    deallocate(temp_board)
+    deallocate(current_board)
+    deallocate(new_board)
 
 contains
 
-    subroutine check_for_steady_state()
-        logical :: equal
-        integer :: i, j
-
-        equal = .true.
-
-        do i=1, nrow
-            do j=1, ncol
-                ! write(*,*) equal, board(i, j), prev_board(i, j)
-                equal = board(i, j) == temp_board(i, j)
-                if (.not. equal) then
-                    steady_state_counter = 0
-                    return
-                end if
-            end do
-        end do
-        if (steady_state_counter == 0) then
-            steady_state_generation = generation_number
-        end if
-
-        steady_state_counter =  steady_state_counter + 1
-
-        if (steady_state_counter > 1) then
-            steady_state = .true.
-            return
-        end if
-        steady_state = .false.
-    end subroutine check_for_steady_state
-
     subroutine evolve_board()
-        integer :: i, j
-        do i=2, nrow-1
-            do j=2, ncol-1
+        integer :: row, col, sum
+
+        do row=2, nrow-1
+            do col=2, ncol-1
                 sum = 0
-                sum = board(i-1, j-1) + board(i, j-1) + board(i+1, j-1) !
-                sum = sum + board(i-1, j) + board(i+1, j)
-                sum = sum + board(i-1, j+1) + board(i, j+1) + board(i+1, j+1)
-                if(board(i,j)==1 .and. sum<=1) then
-                    temp_board(i,j) = 0
-                elseif(board(i,j)==1 .and. sum<=3) then
-                    temp_board(i,j) = 1
-                elseif(board(i,j)==1 .and. sum>=4)then
-                    temp_board(i,j) = 0
-                elseif(board(i,j)==0 .and. sum==3)then
-                    temp_board(i,j) = 1
+                sum = current_board(row-1, col-1) + current_board(row, col-1) + current_board(row+1, col-1) !
+                sum = sum + current_board(row-1, col) + current_board(row+1, col)
+                sum = sum + current_board(row-1, col+1) + current_board(row, col+1) + current_board(row+1, col+1)
+                if(current_board(row,col)==1 .and. sum<=1) then
+                    new_board(row,col) = 0
+                elseif(current_board(row,col)==1 .and. sum<=3) then
+                    new_board(row,col) = 1
+                elseif(current_board(row,col)==1 .and. sum>=4)then
+                    new_board(row,col) = 0
+                elseif(current_board(row,col)==0 .and. sum==3)then
+                    new_board(row,col) = 1
                 endif
             enddo
         enddo
@@ -163,14 +139,31 @@ contains
         return
     end subroutine evolve_board
 
+    subroutine check_for_steady_state()
+        integer :: row, col
+
+        do row=1, nrow
+            do col=1, ncol
+                if (.not. current_board(row, col) == new_board(row, col)) then
+                    steady_state = .false.
+                    return
+                end if
+            end do
+        end do
+        steady_state = .true.
+    end subroutine check_for_steady_state
+
     subroutine draw_board()
-        integer :: i, j
+        integer :: row, col
         character(nrow) :: output
+
+        ! Clear the terminal screen
         call system("clear")
-        do i=1, nrow
+
+        do row=1, nrow
             output = ""
-            do j=1, ncol
-                if (board(i,j) == 1) then
+            do col=1, ncol
+                if (current_board(row,col) == 1) then
                     output = trim(output)//"#"
                 else
                     output = trim(output)//"."
