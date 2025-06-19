fb4722be6284fc2e8d5b03b0345ca8c7686a1969
Connor Aird <c.aird@ucl.ac.uk>
2025-06-19 17:39:28 +0100

    WIP: begin adding episode 4 challenge

---
+++ episodes/4-debugging-a-broken-test/challenge-1/test/test_matrix_transform.f90
@@ -0,0 +1,88 @@
+module test_matrix_transform
+    use testdrive, only : error_type, unittest_type, new_unittest, check
+    use matrix_transforms, only : transpose
+    implicit none
+
+    private
+    public :: test_transpose_testsuite
+
+    type :: test_transpose_params
+        real, dimension(:,:), allocatable :: input
+        real, dimension(:,:), allocatable :: expected_output
+    end type test_transpose_params
+
+contains
+
+    subroutine test_transpose_testsuite(testsuite)
+        type(unittest_type), allocatable, intent(out) :: testsuite(:)
+
+        testsuite =[ &
+            new_unittest("3x3 identity matrix", test_transpose_3x3_identity), &
+            new_unittest("3x3 unsymetric matrix", test_transpose_3x3_unsymetric) &
+        ]
+    end subroutine test_transpose_testsuite
+
+    subroutine test_transpose_3x3_identity(error)
+        type(error_type), allocatable, intent(out) :: error
+
+        type(test_transpose_params) :: params
+
+        allocate(params%input(3,3))
+        allocate(params%expected_output(3,3))
+
+        params%input(1,:) = [1,0,0]
+        params%input(2,:) = [0,1,0]
+        params%input(3,:) = [0,0,1]
+
+        params%expected_output = params%input
+
+        call check_transpose(error, params)
+    end subroutine test_transpose_3x3_identity
+
+    subroutine test_transpose_3x3_unsymetric(error)
+        type(error_type), allocatable, intent(out) :: error
+
+        type(test_transpose_params) :: params
+
+        allocate(params%input(3,3))
+        allocate(params%expected_output(3,3))
+
+        params%input(1,:) = [1,4,0]
+        params%input(2,:) = [6,1,0]
+        params%input(3,:) = [0,3,1]
+
+        params%expected_output(1,:) = [1,6,0]
+        params%expected_output(2,:) = [4,1,3]
+        params%expected_output(3,:) = [0,0,1]
+
+        call check_transpose(error, params)
+    end subroutine test_transpose_3x3_unsymetric
+
+    subroutine check_transpose(error, params)
+        type(error_type), allocatable, intent(out) :: error
+        type(test_transpose_params), intent(in) :: params
+
+        real, dimension(:,:), allocatable :: actual_output
+        integer :: nrow, ncol, row, col
+        character(len=80) :: failure_message
+
+        nrow = size(params%input, 1)
+        ncol = size(params%input, 2)
+
+        allocate(actual_output(nrow, ncol))
+
+        actual_output = params%input
+
+        call transpose(actual_output)
+
+        do row = 1, nrow
+            do col = 1, ncol
+                write(failure_message,'(a,i1,a,i1,a,F3.1,a,F3.1)') "Unexpected value for output(", row, ",", col, "), got ", &
+                    actual_output(row, col), " expected ", params%expected_output(row, col)
+                call check(error, params%expected_output(row, col), actual_output(row, col), failure_message)
+                if (allocated(error)) return
+            end do
+        end do
+    end subroutine check_transpose
+
+end module test_matrix_transform
