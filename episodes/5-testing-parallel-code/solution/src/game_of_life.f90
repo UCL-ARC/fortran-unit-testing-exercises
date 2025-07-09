a896f3456c40ca0c1dcad79ca47f8d6bb71b11e2
Connor Aird <c.aird@ucl.ac.uk>
2025-07-09 19:25:26 +0100

    WIP: implelment MPI parallelism

--- episodes/5-testing-parallel-code/solution/src/game_of_life.f90
+++ episodes/5-testing-parallel-code/solution/src/game_of_life.f90
@@ -5,78 +5,156 @@
 ! Adapted from https://github.com/tuckerrc/game_of_life
 ! =======================================================
 program game_of_life
-    use game_of_life_mod, only : evolve_board, check_for_steady_state, read_model_from_file
-
+    ! allow(C121)
+    use mpi
+    use game_of_life_mod, only : evolve_board, check_for_steady_state, read_model_from_file, exchange_boundaries
     implicit none
 
     !! Board args
     integer, parameter :: max_nrow = 50000, max_ncol = 50000, max_generations = 100
-    integer :: generation_number
-    integer, dimension(:,:), allocatable :: current_board, new_board
-
-    !! Animation args
-    integer, dimension(8) :: date_time_values
-    integer :: mod_ms_step, ms_per_step = 250
-    logical :: steady_state = .false., run_with_animation = .true.
+    integer :: generation_number, local_nrow, local_ncol, global_nrow, global_ncol
+    integer, dimension(:,:), allocatable :: global_board, local_current, local_new
+    logical :: local_steady = .false., global_steady = .false.
 
     !! CLI args
-    integer                       :: argl, i
+    integer :: argl
     character(len=:), allocatable :: cli_arg_temp_store, input_fname
 
     !! IO args
     character(len=:), allocatable :: io_error_message
-    integer :: stat, num_threads
+    integer :: num_ranks_x, num_ranks_y, row_start, col_start, row_end, col_end
 
     !! Timing
     real :: start_time, end_time
 
-    !! Get current_board file path from command line
-    if (command_argument_count() == 1) then
-        call get_command_argument(1, length=argl)
-        allocate(character(argl) :: input_fname)
-        call get_command_argument(1, input_fname)
+    !! MPI args
+    integer :: ierr, rank, nprocs
+    integer :: dims(2), coords(2), cart_comm
+    integer :: nbr_north, nbr_south, nbr_east, nbr_west
+    logical :: periods(2)
+
+    !! Misc
+    integer :: i, j
+
+    ! MPI Init
+    call MPI_Init(ierr)
+    call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
+    call MPI_Comm_size(MPI_COMM_WORLD, nprocs, ierr)
+
+    ! Read input on rank 0
+    if (rank == 0) then
+        if (command_argument_count() == 1) then
+            call get_command_argument(1, length=argl)
+            allocate(character(argl) :: input_fname)
+            call get_command_argument(1, input_fname)
+        else
+            write(*,'(A)') "Error: Invalid input"
+            call get_command_argument(0, length=argl)
+            allocate(character(argl) :: cli_arg_temp_store)
+            call get_command_argument(0, cli_arg_temp_store)
+            write(*,'(A,A,A)') "Usage: ", cli_arg_temp_store, " <input_file_name>"
+            deallocate(cli_arg_temp_store)
+            call MPI_Abort(MPI_COMM_WORLD, 1, ierr)
+            stop
+        end if
+
+        call read_model_from_file(input_fname, max_nrow, max_ncol, global_board, io_error_message)
+
+        if (allocated(io_error_message)) then
+            write (*,*) io_error_message
+            call MPI_Abort(MPI_COMM_WORLD, 1, ierr)
+            stop
+        end if
+
+        global_nrow = size(global_board, 1)
+        global_ncol = size(global_board, 2)
+    end if
+
+    ! Broadcast global dimensions
+    call MPI_Bcast(global_nrow, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
+    call MPI_Bcast(global_ncol, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
+
+    ! Create 2D Cartesian topology
+    dims = 0
+    call MPI_Dims_create(nprocs, 2, dims, ierr)   ! Automatically split into num_ranks_x x num_ranks_y grid
+    periods = [ .false., .false. ]
+    call MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, .true., cart_comm, ierr)
+    call MPI_Cart_coords(cart_comm, rank, 2, coords, ierr)
+    call MPI_Cart_shift(cart_comm, 0, 1, nbr_north, nbr_south, ierr)
+    call MPI_Cart_shift(cart_comm, 1, 1, nbr_west, nbr_east, ierr)
+
+    num_ranks_x = dims(1)
+    num_ranks_y = dims(2)
+
+    ! Local domain sizes
+    local_nrow = global_nrow / num_ranks_x
+    local_ncol = global_ncol / num_ranks_y
+    row_start = coords(1)*local_nrow + 1
+    col_start = coords(2)*local_ncol + 1
+
+    allocate(local_current(local_nrow+2, local_ncol+2))
+    allocate(local_new(local_nrow+2, local_ncol+2))
+    local_current = 0
+    local_new = 0
+
+    ! Scatter global board
+    if (rank == 0) then
+        do i = 1, nprocs - 1
+            call MPI_Cart_coords(cart_comm, i, 2, coords, ierr)
+            row_start = coords(1)*local_nrow + 1
+            col_start = coords(2)*local_ncol + 1
+            call MPI_Send(global_board(row_start:row_start+local_nrow-1, col_start:col_start+local_ncol-1), &
+                          local_nrow*local_ncol, MPI_INTEGER, i, 0, MPI_COMM_WORLD, ierr)
+        end do
+
+        local_current(1:local_nrow, 1:local_ncol) = global_board(1:local_nrow, 1:local_ncol)
     else
-        write(*,'(A)') "Error: Invalid input"
-        call get_command_argument(0, length=argl)
-        allocate(character(argl) :: cli_arg_temp_store)
-        call get_command_argument(0, cli_arg_temp_store)
-        write(*,'(A,A,A)') "Usage: ", cli_arg_temp_store, " <input_file_name>"
-        deallocate(cli_arg_temp_store)
-        stop
-    end if
+        call MPI_Recv(local_current(2:local_nrow+1, 2:local_ncol+1), local_nrow*local_ncol, MPI_INTEGER, &
+                      0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierr)
+    endif
 
-    ! Q1_FIX3: Extract the file IO into a module procedure to allow it to be tested.
-    call read_model_from_file(input_fname, max_nrow, max_ncol, current_board, io_error_message)
+    ! do j = 0, nprocs - 1
+    !     if (j == rank) then
+    !         do i = 1, local_nrow
+    !             write(100+rank,*) local_current(i,:)
+    !         end do
+    !     end if
+    ! end do
 
-    if (allocated(io_error_message)) then
-        write (*,*) io_error_message
-        deallocate(io_error_message)
-        stop
-    end if
-
-    allocate(new_board(size(current_board,1), size(current_board, 2)))
-    new_board = 0
+    call MPI_Barrier(cart_comm, ierr)
+    start_time = MPI_Wtime()
 
     generation_number = 0
+    local_steady = .false.
 
-    call cpu_time(start_time)
-    ! Iterate until we reach a steady state
-    do while(.not. steady_state .and. generation_number < max_generations)
-        call evolve_board(current_board, new_board)
-        call check_for_steady_state(current_board, new_board, steady_state)
+    do while (.not. local_steady .and. generation_number < max_generations)
+        ! Exchange ghost cells with neighbors
+        call exchange_boundaries(local_current, local_nrow, local_ncol, cart_comm, nbr_north, nbr_south, nbr_east, nbr_west)
 
-        current_board = new_board
+        ! call MPI_Finalize(ierr)
+        ! stop
+
+        ! Evolution
+        call evolve_board(local_current, local_new, local_nrow, local_ncol)
+        call check_for_steady_state(local_current, local_new, local_steady, local_nrow, local_ncol)
+
+        call MPI_Allreduce(local_steady, global_steady, 1, MPI_LOGICAL, MPI_LAND, cart_comm, ierr)
+        local_steady = global_steady
+
+        local_current(2:local_nrow, 2:local_ncol) = local_new(2:local_nrow, 2:local_ncol)
+
         generation_number = generation_number + 1
     end do
-    call cpu_time(end_time)
 
-    if (steady_state) then
-        write(*,'(a,i6,a,F5.3)') "Reached steady after ", generation_number, " generations ", end_time - start_time
-    else
-        write(*,'(a,i6,a,F5.3)') "Did NOT Reach steady after ", generation_number, " generations ", end_time - start_time
+    end_time = MPI_Wtime()
+
+    if (rank == 0) then
+        if (local_steady) then
+            print *, "Reached steady state after ", generation_number, " generations. Time: ", end_time - start_time
+        else
+            print *, "Did NOT reach steady state after ", generation_number, " generations. Time: ", end_time - start_time
+        end if
     end if
 
-    deallocate(current_board)
-    deallocate(new_board)
-
+    call MPI_Finalize(ierr)
 end program game_of_life
--- episodes/5-testing-parallel-code/solution/src/game_of_life_mod.f90
+++ episodes/5-testing-parallel-code/solution/src/game_of_life_mod.f90
@@ -1,27 +1,87 @@
 ! Q1_FIX_1: Moving these procedures into a separate file allows them to be used within a test file
 module game_of_life_mod
+    ! allow(C121)
+    use mpi
     implicit none
 
     public
 
 contains
 
-    ! Q1_FIX2: Pass parameters into procedures instead of relying on global state to isolate tests from one another.
+    subroutine exchange_boundaries(board, local_nrow, local_ncol, cart_comm, nbr_north, nbr_south, nbr_east, nbr_west)
+        implicit none
+        !> The number of rows in the local board
+        integer, intent(in) :: local_nrow
+        !> The number of columns in the local board
+        integer, intent(in) :: local_ncol
+        !> The ID of the cartesian communicator containing the ranks
+        integer, intent(in) :: cart_comm
+        !> The current ranks neighbor to the north
+        integer, intent(in) :: nbr_north
+        !> The current ranks neighbor to the south
+        integer, intent(in) :: nbr_south
+        !> The current ranks neighbor to the east
+        integer, intent(in) :: nbr_east
+        !> The current ranks neighbor to the west
+        integer, intent(in) :: nbr_west
+        !> The board to be exchanged
+        integer, dimension(:,:), intent(inout) :: board
+
+        integer :: ierr, rank, mpi_req
+
+        call MPI_Comm_rank(cart_comm, rank, ierr)
+
+        ! Vertical exchange
+        if (nbr_north >= 0) then
+            ! Send top row north
+            call MPI_ISEND(board(2,2:local_ncol), local_ncol, MPI_INTEGER, nbr_north, 0, cart_comm, mpi_req, ierr)
+
+            ! Receive top row from the north
+            CALL MPI_RECV(board(1,2:local_ncol), local_ncol, MPI_INTEGER, nbr_north, 1, cart_comm, MPI_STATUS_IGNORE, ierr)
+        endif
+        if (nbr_south >= 0) then
+            ! Send the bottom row south
+            call MPI_ISEND(board(local_nrow+1,2:local_ncol), local_ncol, MPI_INTEGER, nbr_south, 1, cart_comm, mpi_req, ierr)
+
+            ! Receive the bottom row from the south
+            CALL MPI_RECV( &
+                board(local_nrow+2,2:local_ncol), local_ncol, MPI_INTEGER, nbr_south, 0, cart_comm, MPI_STATUS_IGNORE, ierr)
+        endif
+
+        ! Horizontal exchange
+        if (nbr_west >= 0) then
+            ! Send the left column to the west
+            call MPI_ISEND(board(2:local_nrow,2), local_nrow, MPI_INTEGER, nbr_west, 2, cart_comm, mpi_req, ierr)
+
+            ! Receive the left columns from the west
+            CALL MPI_RECV(board(2:local_nrow,1), local_nrow, MPI_INTEGER, nbr_west, 3, cart_comm, MPI_STATUS_IGNORE, ierr)
+        endif
+        if (nbr_east >= 0) then
+            ! Send the right column east
+            call MPI_ISEND(board(2:local_nrow,local_ncol+1), local_nrow, MPI_INTEGER, nbr_east, 3, cart_comm, mpi_req, ierr)
+
+            ! Receive the right column from the east
+            CALL MPI_RECV( &
+                board(2:local_nrow,local_ncol+2), local_nrow, MPI_INTEGER, nbr_east, 2, cart_comm, MPI_STATUS_IGNORE, ierr)
+        endif
+    end subroutine exchange_boundaries
+
     !> Evolve the board into the state of the next iteration
-    subroutine evolve_board(current_board, new_board)
+    subroutine evolve_board(current_board, new_board, nrow, ncol)
         !> The board as it currently is before this iteration
         integer, dimension(:,:), allocatable, intent(in) :: current_board
         !> The board into which the new state will be stored after this iteration
         integer, dimension(:,:), allocatable, intent(inout) :: new_board
+        !> The number of row elements over which to evolve the board
+        integer, intent(in) :: nrow
+        !> The number of column elements over which to evolve the board
+        integer, intent(in) :: ncol
 
-        integer :: row, col, num_rows, num_cols, sum, num_threads, thread_id
+        integer :: row, col, sum, num_threads, thread_id
 
-        num_rows = size(current_board, 1)
-        num_cols = size(current_board, 2)
-
-        !$omp parallel do default(none) private(row, col, sum) shared(num_rows, num_cols, current_board, new_board)
-        do row=2, num_rows-1
-            do col=2, num_cols-1
+        !$omp parallel do default(none) private(row, col, sum) shared(nrow, ncol, current_board, new_board)
+        do row=3, nrow-1
+            do col=3, ncol-1
 
                 sum = 0
                 sum = current_board(row, col-1)   &
@@ -46,33 +106,62 @@ contains
         !$omp end parallel do
     end subroutine evolve_board
 
-    ! Q1_FIX2: Pass parameters into procedures instead of relying on global state to isolate tests from one another.
     !> Check if we have reached steady state, i.e. current and new board match
-    subroutine check_for_steady_state(current_board, new_board, steady_state)
+    subroutine check_for_steady_state(current_board, new_board, steady_state, nrow, ncol)
         !> The board as it currently is before this iteration
-        integer, dimension(:,:), allocatable, intent(in) :: current_board
+        integer, dimension(:,:), intent(in) :: current_board
         !> The board into which the new state has been stored after this iteration
-        integer, dimension(:,:), allocatable, intent(in) :: new_board
+        integer, dimension(:,:), intent(in) :: new_board
         !> Logical to indicate whether current and new board match
         logical, intent(out) :: steady_state
+        !> The number of row elements over which to check for steady state
+        integer, intent(in) :: nrow
+        !> The number of column elements over which to check for steady state
+        integer, intent(in) :: ncol
 
-        integer :: row, col, num_rows, num_cols
-
-        num_rows = size(current_board, 1)
-        num_cols = size(current_board, 2)
+        integer :: row, col
 
         steady_state = .true.
         !$omp parallel do reduction(.and.: steady_state)
-        do row=1, num_rows
-            do col=1, num_cols
-                if (.not. current_board(row, col) == new_board(row, col)) then
+        do row = 2, nrow+1
+            do col = 2, ncol+1
+                if (current_board(row, col) /= new_board(row, col)) then
                     steady_state = .false.
-                    ! return
                 end if
             end do
         end do
         !$omp end parallel do
     end subroutine check_for_steady_state
+
+    !> Output the current board to the terminal
+    subroutine draw_board(current_board)
+        !> The board as it currently is for this iteration
+        integer, dimension(:,:), allocatable, intent(in) :: current_board
+
+        integer :: row, col, num_rows, num_cols
+        character(len=:), allocatable :: output
+
+        call system("clear")
+
+        num_rows = size(current_board, 1)
+        num_cols = size(current_board, 2)
+
+        allocate(character(num_rows) :: output)
+
+        do row=1, num_rows
+            output = ""
+            do col=1, num_cols
+                if (current_board(row,col) == 1) then
+                    output = trim(output)//"#"
+                else
+                    output = trim(output)//"."
+                endif
+            enddo
+            print *, output
+        enddo
+
+        deallocate(output)
+    end subroutine draw_board
 
     ! Q1_FIX3: Extract the file IO into a module procedure to allow it to be tested.
     !> Populate the a board from the provided file
