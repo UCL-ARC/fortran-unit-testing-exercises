9c0ccb384959cc3e2a48ebed641bce96ddfa5459
connoraird <c.aird@ucl.ac.uk>
2025-07-31 15:54:48 +0100

    Add find_steady_state to episode 2

--- episodes/2-intro-to-fortran-unit-tests/challenge-1/solution/src/game_of_life_mod.f90
+++ episodes/2-intro-to-fortran-unit-tests/challenge-1/solution/src/game_of_life_mod.f90
@@ -7,6 +7,52 @@ module game_of_life_mod
 contains
 
     ! FIX2: Pass parameters into procedures instead of relying on global state to isolate tests from one another.
+    subroutine find_steady_state(animate, steady_state, generation_number, current_board)
+        logical, intent(in) :: animate
+        logical, intent(out) :: steady_state
+        integer, intent(out) :: generation_number
+        integer, dimension(:,:), allocatable, intent(inout) :: current_board
+
+        integer, dimension(:,:), allocatable :: new_board
+
+        !! Board args
+        integer, parameter :: max_generations = 100
+
+        !! Animation args
+        integer, dimension(8) :: date_time_values
+        integer :: mod_ms_step
+        integer, parameter :: ms_per_step = 250
+
+        allocate(new_board(size(current_board,1), size(current_board, 2)))
+        new_board = 0
+
+        ! Clear the terminal screen
+        if (animate) call system ("clear")
+
+        ! Iterate until we reach a steady state
+        steady_state = .false.
+        generation_number = 0
+        mod_ms_step = 0
+        do while(.not. steady_state .and. generation_number < max_generations)
+            ! Advance the simulation in the steps of the requested number of milliosecons
+            if (animate) then
+                call date_and_time(VALUES=date_time_values)
+                mod_ms_step = mod(date_time_values(8), ms_per_step)
+            end if
+
+            if (mod_ms_step == 0) then
+                call evolve_board(current_board, new_board)
+                call check_for_steady_state(current_board, new_board, steady_state)
+                current_board = new_board
+                if (animate) call draw_board(current_board)
+
+                generation_number = generation_number + 1
+            end if
+
+        end do
+    end subroutine find_steady_state
+
+    ! FIX2: Pass parameters into procedures instead of relying on global state to isolate tests from one another.
     !> Evolve the board into the state of the next iteration
     subroutine evolve_board(current_board, new_board)
         !> The board as it currently is before this iteration
